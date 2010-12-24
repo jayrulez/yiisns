b87f4764de4d37a074949ce4afeafb16c87c3519
waprave <waprave@gmail.com>
2010-12-24 20:37:43 +0000

    Should probably commit this since I finally got it working

--- yiisns/protected/widgets/views/postStream.php
+++ yiisns/protected/widgets/views/postStream.php
@@ -1,3 +1,13 @@
 <div id="post-stream-content">
-
+	<div id="posts">
+		<?php if(count($posts)): ?>
+		<?php foreach($posts as $post): ?>
+		<div class="post">
+			<?php echo $post->user->getLink(); ?> <?php echo $post->content; ?>
+		</div>
+		<?php endforeach; ?>
+		<?php else: ?>
+			<?php echo Yii::t('application', 'No posts.'); ?>
+		<?php endif; ?>
+	</div>
 </div>
